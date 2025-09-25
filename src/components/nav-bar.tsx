09c24fe6270f77ff1962a5c16767e1fd54fe6f88
neves <118855875+neveshardd@users.noreply.github.com>
2025-09-25 20:36:12 -0300

    update navbar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import useEmblaCarousel from "embla-carousel-react";
 import { useEffect, useRef, useState } from "react";
 import { Book, CircleUser, Coffee, CreditCard, Dumbbell, Frame, GalleryHorizontal, Heart, Hotel, IceCream, Move, Music, PawPrint, Scissors, Search, Shirt, ShoppingCart, ToolCase, TreeDeciduous, Utensils } from "lucide-react";
+import { Description, Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react";
 
 const categories = [
     { name: 'Cafeterias', icon: Coffee, page: '/cafeterias' },
@@ -31,6 +32,8 @@ const categories = [
 ]
 
 export default function NavBar() {
+    let [isOpen, setIsOpen] = useState(false)
+
     const [emblaRef, emblaApi] = useEmblaCarousel({
         loop: false,
         dragFree: true,
@@ -75,42 +78,64 @@ export default function NavBar() {
     }, [emblaApi, direction]);
 
     return (
-        <nav className="bg-white border-b-3 border-black/10">
-            <div className="flex justify-between items-center p-6 max-w-7xl mx-auto">
-                <Link href="/" className="font-bold text-2xl">Localtop</Link>
-                <div className="hover:bg-black/5 p-2 rounded-full cursor-pointer transition-all">
-                    <CircleUser size={28} />
-                </div>
-            </div>
+        <>
+            <nav className="bg-white border-b-3 border-black/10">
+                <div className="flex justify-between items-center p-6 max-w-6xs mx-auto">
+                    <Link href="/" className="font-bold text-2xl">Localtop</Link>
 
-            <div className="flex flex-col items-center space-y-5 max-w-6xl px-6 mx-auto">
-                <div className="relative w-full max-w-3xl">
-                    <Search className="absolute left-3 md:left-8 top-1/2 -translate-y-1/2 text-gray-400" size={20} />
-                    <input
-                        type="search"
-                        placeholder="Buscar lanchonetes, lojas..."
-                        className="w-full pl-10 p-3.5 border border-black/15 shadow-xl shadow-black/10 md:py-4.5 md:pl-15 rounded-full focus:outline-black/60"
-                    />
-                </div>
-
-                <div className="w-full max-w-6xl overflow-hidden mb-2" ref={emblaRef}>
-                    <div className="flex">
-                        {categories.map((category, index) => {
-                            const Icon = category.icon;
-                            return (
-                                <Link
-                                    key={index}
-                                    href={category.page}
-                                    className="flex flex-col items-center min-w-[100px] px-2 py-3 mx-2 mt-4 rounded-2xl hover:bg-black/5 justify-center"
-                                >
-                                    <Icon size={20} />
-                                    <span className="mt-1 text-sm text-center">{category.name}</span>
-                                </Link>
-                            )
-                        })}
+                    <div className="flex items-center justify-between space-x-2">
+                        <Link href='/' className="hover:bg-black/5 py-2 px-4 rounded-full font-medium hidden md:flex">Cadastre sua Empresa</Link>
+                        <div className="bg-black/5 hover:bg-black/10 p-2 rounded-full cursor-pointer transition-all flex space-x-2">
+                            <CircleUser size={28} />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+
+                <div className="flex flex-col items-center max-w-6xl mx-auto space-y-5 px-6">
+
+                    <button
+                        onClick={() => setIsOpen(true)}
+                        className="flex items-center space-x-2 border rounded-full p-4 w-full max-w-2xl border-black/20 text-black/40 shadow-md"
+                    >
+                        <Search />
+                        <p>Buscar Lanchonete, Hospital, Bar...</p>
+                    </button>
+
+                    <div className="w-full max-w-6xl overflow-hidden mb-4" ref={emblaRef}>
+                        <div className="flex justify-center" >
+                            {categories.map((category, index) => {
+                                const Icon = category.icon;
+                                return (
+                                    <Link
+                                        key={index}
+                                        href={category.page}
+                                        className="flex flex-col items-center min-w-[100px] py-3 mx-2 rounded-2xl hover:bg-black/5 justify-center"
+                                    >
+                                        <Icon size={20} />
+                                        <span className="mt-1 text-sm text-center">{category.name}</span>
+                                    </Link>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+                <DialogBackdrop className="fixed inset-0 bg-black/30" />
+
+                <div className="fixed inset-0 flex items-center justify-center p-4">
+                    <DialogPanel className="max-w-lg space-y-4 bg-white p-12">
+                        <DialogTitle className="font-bold">Deactivate account</DialogTitle>
+                        <Description>This will permanently deactivate your account</Description>
+                        <p>Are you sure you want to deactivate your account? All of your data will be permanently removed.</p>
+                        <div className="flex gap-4">
+                            <button onClick={() => setIsOpen(false)}>Cancel</button>
+                            <button onClick={() => setIsOpen(false)}>Deactivate</button>
+                        </div>
+                    </DialogPanel>
+                </div>
+            </Dialog>
+        </>
     );
 }
