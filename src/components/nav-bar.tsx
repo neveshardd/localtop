42cf17a47d78385121caa07ac79df0328641baf0
neves <118855875+neveshardd@users.noreply.github.com>
2025-09-25 20:54:31 -0300

    Update nav-bar.tsx

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,68 +1,46 @@
 'use client'
 
 import Link from "next/link";
-import useEmblaCarousel from "embla-carousel-react";
-import { useEffect, useRef, useState } from "react";
-import { CircleUser, Coffee, Dumbbell, Heart, Search } from "lucide-react";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { CircleUser, Heart, Search, ShoppingCart, Utensils } from "lucide-react";
 import { Description, Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react";
 
 const categories = [
-    { name: 'Cuidados', icon: Coffee, page: '/cuidados' },
-    { name: 'Restaurantes', icon: Dumbbell, page: '/restaurantes' },
-    { name: 'Mercados', icon: Heart, page: '/mercados' },
+    { name: 'Restaurantes', icon: Utensils, page: '/' },
+    { name: 'Cuidados', icon: Heart, page: '/cuidados' },
+    { name: 'Mercados', icon: ShoppingCart, page: '/mercados' },
 ]
 
 export default function NavBar() {
     let [isOpen, setIsOpen] = useState(false)
-
-    const [emblaRef, emblaApi] = useEmblaCarousel({
-        loop: false,
-        dragFree: true,
-        align: 'start',
-    });
-
-    const autoplayRef = useRef<number | null>(null);
-    const [direction, setDirection] = useState(1);
-
-    useEffect(() => {
-        if (!emblaApi) return;
-
-        const play = () => {
-            if (!emblaApi) return;
-
-            const slides = emblaApi.scrollSnapList();
-            const current = emblaApi.selectedScrollSnap();
-
-            if (current === slides.length - 1) setDirection(-1);
-            if (current === 0) setDirection(1);
-
-            const next = current + direction;
-            emblaApi.scrollTo(next);
-        };
-
-        autoplayRef.current = window.setInterval(play, 2000);
-
-        const stopAutoplay = () => clearInterval(autoplayRef.current!);
-        const startAutoplay = () => {
-            stopAutoplay()
-            autoplayRef.current = window.setInterval(play, 3000)
-        }
-
-        emblaApi.on('pointerDown', stopAutoplay);
-        emblaApi.on('pointerUp', startAutoplay);
-
-        return () => {
-            stopAutoplay();
-            emblaApi.off('pointerDown', stopAutoplay);
-            emblaApi.off('pointerUp', startAutoplay);
-        };
-    }, [emblaApi, direction]);
+    const pathname = usePathname();
 
     return (
         <>
             <nav className="bg-white border-b-3 border-black/10">
                 <div className="flex justify-between items-center p-6 max-w-6xs mx-auto">
                     <Link href="/" className="font-bold text-2xl">Localtop</Link>
+
+                    <div className="max-w-6xl hidden md:flex justify-center items-center">
+                        <div className="flex justify-center space-x-2 ml-28">
+                            {categories.map((category, index) => {
+                                const isActive = pathname === category.page;
+                                return (
+                                    <Link
+                                        key={index}
+                                        href={category.page}
+                                        className="flex space-x-4 items-center min-w-[100px] py-3 rounded-t-2xl justify-center relative"
+                                    >
+                                        <span className="mt-1 text-sm text-center font-medium">{category.name}</span>
+                                        {isActive && (
+                                            <span className="absolute -bottom-1 left-0 right-0 h-1 bg-black" />
+                                        )}
+                                    </Link>
+                                )
+                            })}
+                        </div>
+                    </div>
 
                     <div className="flex items-center justify-between space-x-2">
                         <Link href='/' className="hover:bg-black/5 py-2 px-4 rounded-full font-medium hidden md:flex">Cadastre sua Empresa</Link>
@@ -73,7 +51,6 @@ export default function NavBar() {
                 </div>
 
                 <div className="flex flex-col items-center max-w-6xl mx-auto space-y-5 px-6">
-
                     <button
                         onClick={() => setIsOpen(true)}
                         className="flex items-center space-x-2 border rounded-full p-4 w-full max-w-2xl border-black/20 text-black/40 shadow-md"
@@ -82,8 +59,8 @@ export default function NavBar() {
                         <p>Buscar Lanchonete, Hospital, Bar...</p>
                     </button>
 
-                    <div className="w-full max-w-6xl overflow-hidden mb-4" ref={emblaRef}>
-                        <div className="flex justify-center" >
+                    <div className="w-full max-w-6xl overflow-hidden mb-4 md:hidden">
+                        <div className="flex justify-center space-x-2">
                             {categories.map((category, index) => {
                                 const Icon = category.icon;
                                 return (
